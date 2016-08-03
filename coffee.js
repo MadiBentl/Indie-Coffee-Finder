570a76083b0c80c975306fa28a35f118cf1b86e1
MadiBentley <mb.madisonbentley@gmail.com>
2016-08-03 15:25:43 -0700

    Add scss file and colour palette

--- coffee.js
+++ coffee.js
@@ -2,6 +2,22 @@ $(document).ready(function(){
 	$('h1').css('color', 'red');
 });
 
+getLocation();
+
+var x = document.getElementById("map-container");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
+
+
 
 
 /*#header
