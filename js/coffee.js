31681b66a55fc23feebe2c7e3e30f61c69dbf6e5
MadiBentley <mb.madisonbentley@gmail.com>
2016-08-08 09:31:01 -0700

    added js-store-finder.min.js

--- js/coffee.js
+++ js/coffee.js
@@ -1,11 +1,41 @@
 $(document).ready(function(){
 
 });
+ getLocation();
+
+var location,
+	latt,
+	lon;
+function getLocation() {
+    $.get("http://ipinfo.io", function(location) {
+      console.log(location);
+      
+      location= location.city + location.region;
+      $('.location')
+        .append(location.city + ", ")
+        .append(location.region);
+
+    }, "jsonp");
+
+  }
+function convertLocation(){
+	var geocoder =  new google.maps.Geocoder();
+    geocoder.geocode( { 'address': 'location'}, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            latt= results[0].geometry.location.lat();
+            lon= results[0].geometry.location.lng();
+            console.log(latt + " " + lon) 
+          } else {
+            alert("Something got wrong " + status);
+          }
+    });
+}
+
 var map;
 function initMap() {
-	var myLatLng = {lat: 49.283, lng: -123.120};
+	var myLatLng = {lat: latt, lng: lon};
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 49.283, lng: -123.120},
+        center: {lat: latt, lng: lon},
         scrollwheel: false,
         zoom: 6
     });
