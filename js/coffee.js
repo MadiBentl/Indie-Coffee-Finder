de8f342aedec8ebf3091e155a0c940f0c52d9799
MadiBentley <mb.madisonbentley@gmail.com>
2016-08-11 14:39:49 -0700

    get the places map working properly

--- js/coffee.js
+++ js/coffee.js
@@ -2,54 +2,78 @@ $(document).ready(function(){
 
 });
 
- getLocation();
+var map;
+var infoWindow;
+var service;
 
-var location,
-	latt,
-	lon;
-function getLocation() {
-    $.get("http://ipinfo.io", function(location) {
-      console.log(location);
-      
-      location= location.city + location.region;
-      $('.location')
-        .append(location.city + ", ")
-        .append(location.region);
+function initialize() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: new google.maps.LatLng(47.53772, -122.1153),
+        zoom: 11,
+        styles: [
+            {
+              stylers: [
+                { visibility: 'simplified' }
+              ]
+            },
+            {
+              elementType: 'labels',
+              stylers: [
+                { visibility: 'on' }
+              ]
+            }
+        ]
+    });
 
-    }, "jsonp");
+    infoWindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
 
-  }
-function convertLocation(){
-	var geocoder =  new google.maps.Geocoder();
-    geocoder.geocode( { 'address': 'location'}, function(results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-            latt= results[0].geometry.location.lat();
-            lon= results[0].geometry.location.lng();
-            console.log(latt + " " + lon) 
-          } else {
-            alert("Something got wrong " + status);
-          }
+    google.maps.event.addListenerOnce(map, 'bounds_changed', performSearch);
+}
+
+function performSearch() {
+    var request = {
+        bounds: new google.maps.LatLngBounds(
+        new google.maps.LatLng(47.5204, -122.2047),
+        new google.maps.LatLng(47.5986, -122.0672)),
+        	keyword: 'park',
+			types: ['park']
+        };
+    service.radarSearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status != google.maps.places.PlacesServiceStatus.OK) {
+        alert(status);
+        return;
+    }
+    for (var i = 0, result; result = results[i]; i++) {
+        createMarker(result);
+    }
+}
+
+function createMarker(place) {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        icon: 'http://www.google.com/intl/en_us/mapfiles/ms/micons/yellow-dot.png'
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        service.getDetails(place, function(result, status) {
+            if (status != google.maps.places.PlacesServiceStatus.OK) {
+              	alert(status);
+			  	return;
+            }
+            infoWindow.setContent(result.name);
+            infoWindow.open(map, marker);
+        });
     });
 }
 
-var map;
-function initMap() {
-	var myLatLng = {lat: latt, lng: lon};
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: latt, lng: lon},
-        scrollwheel: false,
-        zoom: 6
-    });
-    var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
-    })
-};
-
-
-
-/*#header
+      initialize();
+      /*#header
 #container
 #map-container
 #location-name*/
